64778476014bd79e239dbe23928186326c8f5473
Mark Palmer <mark.palmer@itsalllive.com>
2019-05-14 10:27:57 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace guessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberToGuess = 0;
+            string message = "hello World";
+            Console.WriteLine(message);
+
+        }
+    }
+}
