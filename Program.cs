f10ec368e840767404d585fb16de5aa51501b563
Mark Palmer <mark.palmer@itsalllive.com>
2019-05-14 10:51:45 -0400

    Added Farewell

--- Program.cs
+++ Program.cs
@@ -6,9 +6,12 @@ namespace guessingGame
     {
         static void Main(string[] args)
         {
-            int numberToGuess = 0;
+            //int numberToGuess = 0;
             string message = "hello World";
             Console.WriteLine(message);
+            string farwell = "goodbye World";
+            Console.WriteLine(farwell);
+
 
         }
     }
